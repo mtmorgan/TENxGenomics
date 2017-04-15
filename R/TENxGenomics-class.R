8a25c93435998f9ae56cb99995ed4a9ba1abbef6
Martin Morgan <martin.morgan@roswellpark.org>
2017-04-15 11:54:28 -0400

    dim(), dimnames(), [,TENxGenomics,ANY,ANY-method
    - support for integer, numeric, and missing subset only, currently

--- R/TENxGenomics-class.R
+++ R/TENxGenomics-class.R
@@ -25,16 +25,20 @@
     seq_len(n)
 }
 
-.h5_colidx <-
-    function(h5f)
-{
-    .h5_dimidx(h5f, "/mm10/barcodes")
-}
+.h5_colidx <- function(h5f) .h5_dimidx(h5f, "/mm10/barcodes")
 
-.h5_rowidx <-
-    function(h5f)
+.h5_rowidx <- function(h5f) .h5_dimidx(h5f, "/mm10/genes")
+
+.h5_dimnames <-
+    function(h5f, rowidx, colidx)
 {
-    .h5_dimidx(h5f, "/mm10/genes")
+    h5d <- H5Dopen(h5f, "/mm10/barcodes")
+    on.exit(H5Dclose(h5d))
+browser()
+    list(
+        h5read(h5f, "/mm10/genes", colidx),
+        h5read(h5f, "/mm10/barcodes", colidx)
+    )
 }
 
 #' @rdname TENxGenomics-class
@@ -63,12 +67,111 @@ TENxGenomics <-
     )
 
     h5f <- H5Fopen(h5path)
+    on.exit(H5Fclose(h5f))
     colidx <- .h5_colidx(h5f)
     rowidx <- .h5_rowidx(h5f)
-    H5Fclose(h5f)
 
     .TENxGenomics(h5path = h5path, colidx = colidx, rowidx = rowidx)
 }
+
+##
+## dim, dimnames
+##
+
+#' @rdname TENxGenomics-class
+#'
+#' @aliases dim,TENxGenomics-method
+#'
+#' @exportMethod dim
+setMethod("dim", "TENxGenomics",
+    function(x)
+{
+    c(length(.rowidx(x)), length(.colidx(x)))
+})
+
+#' @rdname TENxGenomics-class
+#'
+#' @aliases dimnames,TENxGenomics-method
+#'
+#' @exportMethod dimnames
+setMethod("dimnames", "TENxGenomics",
+    function(x)
+{
+    dim <- dim(x)
+
+    h5f <- H5Fopen(.h5path(x))
+    on.exit(H5Fclose(h5f))
+
+    list(
+        h5read(h5f, "/mm10/genes", index=list(.rowidx(x))),
+        h5read(h5f, "/mm10/barcodes", index=list(.colidx(x)))
+    )
+})
+
+##
+## Subset
+##
+
+.subset_i_from_character <-
+    function(x, i)
+{
+}
+
+.subset_i_as_integer <-
+    function(x, idx)
+{
+    if (missing(idx))
+        return(.rowidx(x))
+    switch(
+        class(idx),
+        integer = idx,
+        numeric = as.integer(idx),
+        character = .subset_i_from_character(x, idx),
+        logical = .subset_i_from_logical(x, idx)
+    )
+}
+
+.subset_j_as_integer <-
+    function(x, idx)
+{
+    if (missing(idx))
+        return(.colidx(x))
+
+    switch(
+        class(idx),
+        integer = idx,
+        numeric = as.integer(idx),
+        character = .subset_i_from_character(x, idx),
+        logical = .subset_i_from_logical(x, idx)
+    )
+}
+
+#' @rdname TENxGenomics-class
+#'
+#' @aliases [,TENxGenomics,ANY,ANY-method
+#'
+#' @param x A \code{TENxGenomics-class} instance.
+#'
+#' @param i integer() or numeric() index into rows of \code{x}.
+#'
+#' @param j integer() or numeric() index into columns of \code{x}.
+#'
+#' @param drop logical(1) TRUE only.
+#'
+#' @param ... Additional arguments, ignored.
+#' 
+#' @exportMethod [
+setMethod("[", c("TENxGenomics", "ANY", "ANY"),
+    function(x, i, j, ..., drop = TRUE)
+{
+    stopifnot(identical(drop, TRUE))
+
+    i <- .subset_i_as_integer(x, i)
+    j <- .subset_j_as_integer(x, j)
+    stopifnot(all(i %in% .rowidx(x)), all(j %in% .colidx(x)))
+
+    initialize(x, rowidx = .rowidx(x)[i], colidx = .colidx(x)[j])
+})
 
 #' @rdname TENxGenomics-class
 #'
@@ -83,7 +186,7 @@ setMethod("show", "TENxGenomics",
     cat(
         "class:", class(object),
         "\nh5path:", .h5path(object),
-        "\ndim:", length(.rowidx(object)), "x", length(.colidx(object)),
+        "\ndim:", nrow(object), "x", ncol(object),
         "\n"
     )
 })
