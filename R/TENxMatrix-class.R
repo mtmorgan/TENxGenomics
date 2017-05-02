2bb67c9802cf7f2507e89ff8c3ad8b0474df184d
Martin Morgan <martin.morgan@roswellpark.org>
2017-05-02 19:09:30 -0400

    revise TENxMatrix-class documentation
    - more closely associate documentation chunks with exported symbols
    - document arguments
    - use 'h5path' rather than 'file' for path to hdf5 file
    - more liberal spacing

--- R/TENxMatrix-class.R
+++ R/TENxMatrix-class.R
@@ -4,24 +4,21 @@
 
 #' @rdname TENxMatrix-class
 #'
+#' @title TENxMatrix objects
+#'
 #' @name TENxMatrix-class
 #'
-#' @title TENxMatrix objects
+#' @aliases TENxMatrixSeed-class
 #'
 #' @description A container for representing 10xGenomics data.
 #'     TENxMatrix extends \link{DelayedArray} so all the operations
 #'     available on \link{DelayedArray} objects work on TENxMatrix objects.
 #'
-#' @exportClass TENxMatrixSeed TENxMatrix
-#' @export TENxMatrix
-#' @exportMethod dim dimnames coerce
 #' @import methods BiocGenerics S4Vectors IRanges DelayedArray HDF5Array
+#'
 #' @importFrom tools file_path_as_absolute
-#' @aliases TENxMatrixSeed TENxMatrix
-#' @aliases dim,TENxMatrixSeed-method dimnames,TENxMatrixSeed-method
-#' @aliases DelayedArray,TENxMatrixSeed-method
-
-
+#'
+#' @exportClass TENxMatrixSeed
 setClass("TENxMatrixSeed",
     representation(
         file="character",     # Absolute path to the HDF5 file so the object
@@ -72,12 +69,24 @@ setClass("TENxMatrixSeed",
 .get_shape <- function(file, group, idx=NULL)
     .get_TENx_component(file, group, "shape", idx=idx)
 
-TENxMatrixSeed <- function(file, group="mm10")
+#' @rdname TENxMatrix-class
+#'
+#' @param h5path character(1) path to the 10xGenomics hdf5 file, or a
+#'     \code{TENxMatrixSeed} instance.
+#'
+#' @param group character(1) group (e.g., \dQuote{mm10}) containing
+#'     scRNA-seq data; only allowed when \code{h5path} is a
+#'     character(1).
+#'
+#' @return \code{TENxMatrixSeed()} returns a TENxMatrixSeed instance.
+#'
+#' @export TENxMatrixSeed
+TENxMatrixSeed <- function(h5path, group="mm10")
 {
-    if (!isSingleString(file))
-        stop(wmsg("'file' must be a single string specifying the path to ",
+    if (!isSingleString(h5path))
+        stop(wmsg("'h5path' must be a single string specifying the path to ",
                   "the HDF5 file where the 10xGenomics data is located"))
-    file <- file_path_as_absolute(file)
+    h5path <- file_path_as_absolute(h5path)
     if (!isSingleString(group))
         stop(wmsg("'group' must be a single string specifying the name ",
                   "of the group in the HDF5 file containing the ",
@@ -86,37 +95,50 @@ TENxMatrixSeed <- function(file, group="mm10")
         stop(wmsg("'group' cannot be the empty string"))
 
     ## dim
-    dim <- .get_shape(file, group)
+    dim <- .get_shape(h5path, group)
     stopifnot(length(dim) == 2L)
 
     ## dimnames
-    rownames <- .get_genes(file, group)
+    rownames <- .get_genes(h5path, group)
     stopifnot(length(rownames) == dim[[1L]])
-    colnames <- .get_barcodes(file, group)
+    colnames <- .get_barcodes(h5path, group)
     stopifnot(length(colnames) == dim[[2L]])
     dimnames <- list(rownames, colnames)
 
     ## col_ranges
-    data_len <- HDF5Array:::h5dim(file, paste0(group, "/data"))
+    data_len <- HDF5Array:::h5dim(h5path, paste0(group, "/data"))
     stopifnot(length(data_len) == 1L)
-    indices_len <- HDF5Array:::h5dim(file, paste0(group, "/indices"))
+    indices_len <- HDF5Array:::h5dim(h5path, paste0(group, "/indices"))
     stopifnot(identical(data_len, indices_len))
-    indptr <- .get_indptr(file, group)
+    indptr <- .get_indptr(h5path, group)
     stopifnot(length(indptr) == dim[[2L]] + 1L,
               indptr[[1L]] == 0L,
               indptr[[length(indptr)]] == data_len)
     col_ranges <- data.frame(start=indptr[-length(indptr)] + 1,
                              width=as.integer(diff(indptr)))
 
-    new2("TENxMatrixSeed", file=file,
+    new2("TENxMatrixSeed", file=h5path,
                            group=group,
                            dim=dim,
                            dimnames=dimnames,
                            col_ranges=col_ranges)
 }
 
+#' @rdname TENxMatrix-class
+#'
+#' @aliases dim,TENxMatrixSeed-method
+#'
+#' @param x A \code{TENxMatrix-class} or \code{TENxMatrixSeed-class}
+#'     object.
+#'
+#' @exportMethod dim
 setMethod("dim", "TENxMatrixSeed", function(x) x@dim)
 
+#' @rdname TENxMatrix-class
+#'
+#' @aliases dimnames,TENxMatrixSeed-method
+#'
+#' @exportMethod dimnames
 setMethod("dimnames", "TENxMatrixSeed", function(x) x@dimnames)
 
 ### S4Vectors:::fancy_mseq() does not accept 'offset' of type double yet so
@@ -161,6 +183,9 @@ setMethod("subset_seed_as_array", "TENxMatrixSeed",
     .subset_TENxMatrixSeed_as_array
 )
 
+#' @rdname TENxMatrix-class
+#'
+#' @exportClass TENxMatrix
 setClass("TENxMatrix", contains="DelayedMatrix")
 
 .validate_TENxMatrix <- function(x)
@@ -174,22 +199,35 @@ setClass("TENxMatrix", contains="DelayedMatrix")
 
 setValidity2("TENxMatrix", .validate_TENxMatrix)
 
+#' @rdname TENxMatrix-class
+#'
+#' @aliases DelayedArray,TENxMatrixSeed-method
+#'
+#' @param seed A \code{TENxMatrixSeed} instance.
+#'
+#' @exportMethod DelayedArray
 setMethod("DelayedArray", "TENxMatrixSeed",
     function(seed) DelayedArray:::new_DelayedArray(seed, Class="TENxMatrix")
 )
 
 ### Works directly on a TENxMatrixSeed object, in which case it must be called
 ### with a single argument.
-TENxMatrix <- function(file, group="mm10")
+#' @rdname TENxMatrix-class
+#'
+#' @return \code{TENxMatrix()} returns a TENxMatrix instance;
+#'     \code{dim()} and \code{dimnames()} and all other
+#'     \code{DelayedArray} methods operate on this instance.
+#'
+#' @export
+TENxMatrix <- function(h5path, group="mm10")
 {
-    if (is(file, "TENxMatrixSeed")) {
+    if (is(h5path, "TENxMatrixSeed")) {
         if (!missing(group))
             stop(wmsg("TENxMatrix() must be called with a single argument ",
                       "when passed a TENxMatrixSeed object"))
-        seed <- file
+        seed <- h5path
     } else {
-        seed <- TENxMatrixSeed(file, group)
+        seed <- TENxMatrixSeed(h5path, group)
     }
     DelayedArray(seed)
 }
-
